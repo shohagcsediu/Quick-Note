53ae03e838fd96fca532967fc57e57d8ab744e36
shohagcsediu <shohagcsediu@gmail.com>
2019-08-04 11:16:05 +0600

    second commit

--- app/src/main/java/com/shohagapp/quicknote/MainActivity.java
+++ app/src/main/java/com/shohagapp/quicknote/MainActivity.java
@@ -1,29 +1,61 @@
 package com.shohagapp.quicknote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.shohagapp.quicknote.adapter.NoteAdapter;
+import com.shohagapp.quicknote.model.Note;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText title, description;
     Button save;
     RecyclerView recyclerView;
+    NoteAdapter adapter;
+    ArrayList<Note> notes =new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Initialization();
+        Initialize();
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (title.getText().toString().equals("")||description.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Insert", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    String mTitle, mDescription;
+                    mTitle = title.getText().toString();
+                    mDescription = description.getText().toString();
+
+                    Note temp = new Note(mTitle,mDescription);
+                    notes.add(temp);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 
-    private void Initialization() {
+    private void Initialize() {
         title = findViewById(R.id.titleET);
         description = findViewById(R.id.descET);
         save = findViewById(R.id.saveET);
-
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new NoteAdapter(notes);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/shohagapp/quicknote/adapter/NoteAdapter.java
+++ app/src/main/java/com/shohagapp/quicknote/adapter/NoteAdapter.java
@@ -1,5 +1,6 @@
 package com.shohagapp.quicknote.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,23 +8,41 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.shohagapp.quicknote.R;
+import com.shohagapp.quicknote.model.Note;
+
+import java.util.ArrayList;
 
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.viewHolder> {
+
+    private ArrayList<Note> mynote;
+
+    public NoteAdapter(ArrayList<Note> myNote) {
+        this.mynote = myNote;
+    }
 
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item,parent,false);
+        return new viewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        Note current = mynote.get(position);
 
+        holder.nTitle.setText(current.getmTitle());
+        holder.nDescription.setText(current.getmDescription());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if(mynote.isEmpty()){
+            return 0;
+        }else {
+            return mynote.size();
+        }
+
     }
 
     public class viewHolder extends RecyclerView.ViewHolder{
