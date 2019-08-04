aac02fbce6f2d82a86efcad95e429b4713c83df9
shohagcsediu <shohagcsediu@gmail.com>
2019-08-04 10:13:03 +0600

    first commit

--- app/src/main/java/com/shohagapp/quicknote/adapter/NoteAdapter.java
+++ app/src/main/java/com/shohagapp/quicknote/adapter/NoteAdapter.java
@@ -1,4 +1,39 @@
 package com.shohagapp.quicknote.adapter;
 
-public class NoteAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.shohagapp.quicknote.R;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.viewHolder> {
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        TextView nTitle, nDescription;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            nTitle =itemView.findViewById(R.id.titleTV);
+            nDescription =itemView.findViewById(R.id.descTV);
+        }
+    }
 }
--- app/src/main/java/com/shohagapp/quicknote/model/Note.java
+++ app/src/main/java/com/shohagapp/quicknote/model/Note.java
@@ -1,4 +1,27 @@
 package com.shohagapp.quicknote.model;
 
 public class Note {
+    private String mTitle;
+    private String mDescription;
+
+    public Note(String mTitle, String mDescription) {
+        this.mTitle = mTitle;
+        this.mDescription = mDescription;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
 }
